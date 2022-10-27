69a7f5f67980f58570b9bdeb8c63e509e0f3039f
Bruno Negri <b.negri_@hotmail.com>
2022-10-27 19:17:56 -0300

    matchs progress

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -7,7 +7,14 @@ export default class MatchesController {
   }
 
   async getAll(req: Request, res: Response) {
-    const teams = await this.service.getAll();
-    return res.status(200).json(teams);
+    const { inProgress } = req.query;
+    let matches;
+    if (inProgress) {
+      const boolProgress = inProgress === 'true';
+      matches = await this.service.getByProgress(boolProgress);
+    } else {
+      matches = await this.service.getAll();
+    }
+    return res.status(200).json(matches);
   }
 }
--- app/backend/src/services/MatchesServices.ts
+++ app/backend/src/services/MatchesServices.ts
@@ -7,6 +7,18 @@ export default class MatchesServices {
 
   constructor() {
     this.getAll = this.getAll.bind(this);
+    this.getByProgress = this.getByProgress.bind(this);
+  }
+
+  async getByProgress(progress?: boolean) {
+    const matches = await this.modelMatches.findAll({
+      include: [
+        { model: this.modelTeams, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: this.modelTeams, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+      where: { inProgress: progress },
+    });
+    return matches;
   }
 
   async getAll() {
